23820655bedf8d73981d0545d130520b4de56e61
loppyy100 <tobiadepoju999@gmail.com>
2026-07-21 00:02:27 +0800

    v0.2-0.3: fix overwrite bug (append to list), add date to each entry

--- journal.py
+++ journal.py
@@ -1,8 +1,25 @@
+
 import json
+import os
+from datetime import date
+
 
 def add_entry():
-    text = input("what happened today ? :  ")
-    entry = {"text": text}
-    with open ("journal.json" , "w") as f:
-        json.dump(entry , f)
+    text = input("What happened today? ")
+    entry = {"date": str(date.today()), "text": text    }
+
+    if os.path.exists("journal.json"):
+        with open("journal.json", "r") as f:
+            entries = json.load(f)
+    else:
+        entries = []
+
+    entries.append(entry)
+
+    with open("journal.json", "w") as f:
+        json.dump(entries, f)
+
 add_entry()
+
+
+
